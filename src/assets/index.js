9470de4477cfd4bd54cf07622cff70fb1d5490a0
Zubair Alam <zubairalam@Zubairs-MacBook-Air.local>
2025-01-16 19:15:20 +0530

    prifile image has been changed

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,7 +6,8 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import profile_pic from './Jubar_Alam_pro_pic.png'
+// import profile_pic from './Jubar_Alam_pro_pic.png'
+import profile_pic from "./Zubair_Alam_Portfolio_image.jpg"
 import css from "./tech/css.png";
 import docker from "./tech/docker.png";
 import figma from "./tech/figma.png";
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,23 +12,24 @@ const Hero = () => {
     );
   };
   return (
-    <section className=" flex relative w-full h-screen mx-auto ">
+    <section className=" flex relative w-full h-screen mx-auto min-h-screen overflow-hidden ">
       <div
-        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5  `}
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5 flex-wrap `}
       >
-        <div className="flex flex-col justify-center items-center mt-5">
+        <div className="flex flex-col flex-nowrap justify-center items-center mt-5 min-w-[300px border-3  border-cyan-500]">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
           <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            This is <span className="text-[#915eff]">Jubar</span>
+            This is <span className="text-[#915eff]">Jubair</span>
           </h1>
           <p className={`${styles.heroSubText} text-white-100 mt-2 `}>
             {" "}
             MERN Stack Developer <br className="sm:block hidden" /> Frontend |
             Backend
           </p>
+
 
           <button
             type="submit"
@@ -41,7 +42,7 @@ const Hero = () => {
         {/* Fixme: i have to fix 3d computer component <ComputersCanvas />  */}
 
         <div className="flex flex-end  bg-tertiary rounded-t-full shadow-xl  hover:drop-shadow-2xl mx-auto mt-5 ">
-          <img src={profile_pic} alt="Profile Picture" className="w-[250px] rounded-xl" />
+          <img src={profile_pic} alt="Profile Picture" className="max-w-[300px] rounded-xl" />
         </div>
       </div>
     </section>
